f939ee550340f51db197a0582d16f84489d186b1
pantxisto <jonandonicastelo@outlook.com>
2022-07-25 18:42:23 +0200

    final commit

--- src/app/pets-template-driven/pets-template-driven.component.ts
+++ src/app/pets-template-driven/pets-template-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Pet } from './pet.model';
 
 @Component({
@@ -17,16 +18,18 @@ export class PetsTemplateDrivenComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onSubmit(formValue: Pet) {
-    // Two ways of getting form values
+  onSubmit(petForm: FormGroup) {
+    if (petForm.valid) {
+      // Two ways of getting form values
 
-    // First one passing as parameter from html. Property names coincide with name attribute of html tags.
-    // Check that name property name appears as passedName con console
-    console.log(formValue);
+      // First one passing as parameter from html. Property names coincide with name attribute of html tags.
+      // Check that name property name appears as passedName con console
+      console.log(petForm.value);
 
-    // Second one getting the binded model to the form
-    console.log(this.actualPet)
+      // Second one getting the binded model to the form
+      console.log(this.actualPet)
 
-    // Property values must coincide but property names can vary depending on the name attributes
+      // Property values must coincide but property names can vary depending on the name attributes
+    }
   }
 }
--- src/app/users-reactive/users-reactive.component.ts
+++ src/app/users-reactive/users-reactive.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users-reactive',
   templateUrl: './users-reactive.component.html',
-  styleUrls: ['./users-reactive.component.scss']
+  styleUrls: ['./users-reactive.component.scss'],
 })
 export class UsersReactiveComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  userForm: FormGroup;
+  videogames: string[];
+  constructor() {
+    this.userForm = new FormGroup({
+      name: new FormControl('Juanjo', Validators.required),
+      father: new FormControl('Manolo'),
+      videogames: new FormControl(
+        ['Metroid Prime', 'Animal Crossing'],
+        Validators.required
+      ),
+    });
+    this.videogames = [
+      'Metroid Prime',
+      'Super Mario',
+      'Animal Crossing',
+      'Assasins Creed',
+    ];
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    }
+  }
+
+  onClear() {
+    this.userForm.reset();
+  }
+
+  findControl(controlName: string) {
+    return this.userForm.get(controlName) as FormControl;
+  }
 }
